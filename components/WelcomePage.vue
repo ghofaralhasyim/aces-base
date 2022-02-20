664cb3edec167e9a9c7259065f68898d7c94fc19
Abdul Ghofar Alhasyim <abdul.ghofar@student.umn.ac.id>
2022-02-20 19:43:30 +0700

    fix dependencies

---
+++ components/WelcomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Hello World</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  background-color: $dark;
+}
+</style>
